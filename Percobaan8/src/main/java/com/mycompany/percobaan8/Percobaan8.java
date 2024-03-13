911b38cdb02edae56b92b5fd3b98834b9e737c2b
latifah <latifahtnadiyah9@gmail.com>
2024-03-13 11:47:28 +0700

    push

---
+++ Percobaan8/src/main/java/com/mycompany/percobaan8/Percobaan8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.percobaan8;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Percobaan8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
